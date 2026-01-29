08ba6aa5e3af4739330dc2bc1635ba6cc0cad511
JunPyo0117 <vksehfkdydy@naver.com>
2026-01-29 23:10:09 +0900

    Comment out unused Prisma-related code in PrismaService for improved clarity and maintainability.

--- src/common/prisma.service.ts
+++ src/common/prisma.service.ts
@@ -1,31 +1,27 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
-import { envVariableKeys } from './const/env.const';
+// import { ConfigService } from '@nestjs/config';
+// import { PrismaClient } from '@prisma/client';
+// import { PrismaPg } from '@prisma/adapter-pg';
+// import { Pool } from 'pg';
+// import { envVariableKeys } from './const/env.const';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  constructor(private readonly configService: ConfigService) {
-    // Prisma 7에서는 adapter를 사용해야 함
-    const dbUrl = configService.get<string>(envVariableKeys.dbUrl);
-    const pool = new Pool({ connectionString: dbUrl });
-    const adapter = new PrismaPg(pool);
-
-    super({
-      adapter,
-    });
-  }
-
-  async onModuleInit() {
-    await this.$connect();
-  }
-
-  async onModuleDestroy() {
-    await this.$disconnect();
-  }
+//   extends PrismaClient
+//   implements OnModuleInit, OnModuleDestroy
+export class PrismaService {
+  //   constructor(private readonly configService: ConfigService) {
+  //     // Prisma 7에서는 adapter를 사용해야 함
+  //     const dbUrl = configService.get<string>(envVariableKeys.dbUrl);
+  //     const pool = new Pool({ connectionString: dbUrl });
+  //     const adapter = new PrismaPg(pool);
+  //     super({
+  //       adapter,
+  //     });
+  //   }
+  //   async onModuleInit() {
+  //     await this.$connect();
+  //   }
+  //   async onModuleDestroy() {
+  //     await this.$disconnect();
+  //   }
 }
